21b6a82d98619ce5dcb1ee087b90d20cdc249b2e
Matan Levy <levymatanlevy@gmail.com>
2023-08-18 11:05:33 +0300

    [STYLE] apply to L298N

--- src/move/L298N.cpp
+++ src/move/L298N.cpp
@@ -26,7 +26,7 @@ L298N::L298N(uint8_t pinIN1, uint8_t pinIN2) {
   _pinEnable = -1;
   _pinIN1 = pinIN1;
   _pinIN2 = pinIN2;
-  _pwmVal = 255;  // It's always at the max speed due to jumper on module
+  _pwmVal = 255; // It's always at the max speed due to jumper on module
   _isMoving = false;
   _canMove = true;
   _lastMs = 0;
@@ -36,13 +36,9 @@ L298N::L298N(uint8_t pinIN1, uint8_t pinIN2) {
   pinMode(_pinIN2, OUTPUT);
 }
 
-void L298N::setSpeed(unsigned short pwmVal) {
-  _pwmVal = pwmVal;
-}
+void L298N::setSpeed(unsigned short pwmVal) { _pwmVal = pwmVal; }
 
-unsigned short L298N::getSpeed() {
-  return this->isMoving() ? _pwmVal : 0;
-}
+unsigned short L298N::getSpeed() { return this->isMoving() ? _pwmVal : 0; }
 
 void L298N::forward() {
   digitalWrite(_pinIN1, LOW);
@@ -66,37 +62,36 @@ void L298N::backward() {
 
 void L298N::run(L298N::Direction direction) {
   switch (direction) {
-    case BACKWARD:
-      this->backward();
-      break;
-    case FORWARD:
-      this->forward();
-      break;
-    case STOP:
-      this->stop();
-      break;
+  case BACKWARD:
+    this->backward();
+    break;
+  case FORWARD:
+    this->forward();
+    break;
+  case STOP:
+    this->stop();
+    break;
   }
 }
 
 // Timing and callback
 
-void L298N::runFor(unsigned long delay,
-                   L298N::Direction direction,
+void L298N::runFor(unsigned long delay, L298N::Direction direction,
                    CallBackFunction callback) {
   if ((_lastMs == 0) && _canMove) {
     _lastMs = millis();
 
     switch (direction) {
-      case FORWARD:
-        this->forward();
-        break;
-      case BACKWARD:
-        this->backward();
-        break;
-      case STOP:
-      default:
-        this->stop();
-        break;
+    case FORWARD:
+      this->forward();
+      break;
+    case BACKWARD:
+      this->backward();
+      break;
+    case STOP:
+    default:
+      this->stop();
+      break;
     }
   }
 
@@ -117,17 +112,13 @@ void L298N::forwardFor(unsigned long delay, CallBackFunction callback) {
   this->runFor(delay, FORWARD, callback);
 }
 
-void L298N::forwardFor(unsigned long delay) {
-  this->runFor(delay, FORWARD);
-}
+void L298N::forwardFor(unsigned long delay) { this->runFor(delay, FORWARD); }
 
 void L298N::backwardFor(unsigned long delay, CallBackFunction callback) {
   this->runFor(delay, BACKWARD, callback);
 }
 
-void L298N::backwardFor(unsigned long delay) {
-  this->runFor(delay, BACKWARD);
-}
+void L298N::backwardFor(unsigned long delay) { this->runFor(delay, BACKWARD); }
 
 void L298N::stop() {
   digitalWrite(_pinIN1, LOW);
@@ -139,16 +130,10 @@ void L298N::stop() {
   _isMoving = false;
 }
 
-void L298N::reset() {
-  _canMove = true;
-}
+void L298N::reset() { _canMove = true; }
 
-boolean L298N::isMoving() {
-  return _isMoving;
-}
+boolean L298N::isMoving() { return _isMoving; }
 
-L298N::Direction L298N::getDirection() {
-  return _direction;
-}
+L298N::Direction L298N::getDirection() { return _direction; }
 
 void L298N::fakeCallback() {}
